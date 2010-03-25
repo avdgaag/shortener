bca93592402058cf60363caa840ab8054ae6b3c6
Arjan van der Gaag <info@agwebdesign.nl>
2010-03-25 08:49:47 +0100

    Added some tests

--- test_shortener.rb
+++ test_shortener.rb
@@ -13,12 +13,25 @@ class TestShortener < Test::Unit::TestCase
   def test_home_renders_content
     get '/'
     assert last_response.ok?
-    assert_match /Shortener/, last_response.body
+    assert_match /URL Shortener/, last_response.body
   end
 
   def test_shorten_redirects_to_info
     post '/shorten', :url => 'http://example.com'
-    assert_equal 302, last_response.status
-    assert_match %r{/info/.*}, last_response['Location']
+    assert last_response.redirect?
+    assert_match %r{/info/.*}, last_response.location
+  end
+
+  def test_shows_new_and_old_urls
+    post '/shorten', :url => 'http://example.com'
+    follow_redirect!
+    assert last_response.ok?
+    assert_match /example.com/, last_response.body
+    assert_match /shortener.arjanvandergaag.nl\/.+/, last_response.body
+  end
+
+  def test_should_not_shrink_non_urls
+    post '/shorten', :url => 'foo'
+    assert last_response.server_error?
   end
 end
